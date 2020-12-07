c3692a210895a3f61684a8b1659d564ccdf77d1f
Cameron Batt <vespertilian@gmail.com>
2020-12-07 18:28:11 +1100

    Attempt to get layers working

--- apps/api/src/app/hello/hello.ts
+++ apps/api/src/app/hello/hello.ts
@@ -1,6 +1,8 @@
+import { adjectives, animals, colors, uniqueNamesGenerator } from 'unique-names-generator';
 export const handler = async () => {
+  const name = uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals] })
   return {
     statusCode: 200,
-    body: JSON.stringify({ payload: 'heavy' }),
+    body: JSON.stringify({ payload: `Unique name is ${name}` }),
   };
 };
---
+++ apps/api/src/test-layer/index.ts
@@ -0,0 +1,1 @@
+require('unique-names-generator');
